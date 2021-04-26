402f5b5709e76d8d89f812b7ca5120cdfc719f95
Wayne <tsun0227@yahoo.com.tw>
2021-04-26 15:43:01 +0800

    carousel flix

--- src/components/homapage/HP-component/page1component/Page1Carousel.js
+++ src/components/homapage/HP-component/page1component/Page1Carousel.js
@@ -3,34 +3,30 @@ import React, { useEffect, useState } from "react";
 function Page1Carousel(props) {
   const { img1, img2, img3, img4 } = props;
 
-  const display = `<div class="d-flex page1img-wrap position-absolute">
-  <img src=${img1} alt="" />
-  <img src=${img2} alt="" />
-  <img src=${img3} alt="" />
-  <img src=${img4} alt="" />
-  <img src=${img1} alt="" />
-</div>`;
-
   useEffect(() => {
     const slr = (id) => document.querySelector(id);
     let newIndex = 1;
     let slide = -60;
 
     const interval = setInterval(() => {
-      slr(".page1img-wrap").style.transform = `translate(${slide}vw, 0px)`;
-      newIndex++;
-      slide = newIndex * -60;
-
-      if (newIndex === 5) {
-        newIndex = 0;
-        slide = 0;
-
-        setTimeout(() => {
-          slr(".page1right-wrap").innerHTML = display;
-          slr(".page1img-wrap").style.transform = `translate(${slide}vw, 0px)`;
-        }, 1200);
-      }
-    }, 1500);
+      const styles = document.querySelectorAll(".carousel-img");
+      // console.log(styles);
+      styles.forEach((v, i) => {
+        let opacity = +v.style.opacity;
+        let left = +v.style.left.split("vw")[0];
+        console.log(opacity);
+        if (opacity != 0) {
+          left === 0
+            ? (v.style.opacity = "0")
+            : (v.style.left = left - 60 + "vw");
+        } else if (left === 0) {
+          v.style.left = "120vw";
+        } else {
+          v.style.left = left - 60 + "vw";
+          v.style.opacity = " 0.8";
+        }
+      });
+    }, 1800);
 
     return () => {
       clearInterval(interval);
@@ -39,13 +35,32 @@ function Page1Carousel(props) {
 
   return (
     <>
-      <div className="page1right-wrap position-relative">
-        <div className="d-flex page1img-wrap position-absolute">
-          <img src={img1} alt="" />
-          <img src={img2} alt="" />
-          <img src={img3} alt="" />
-          <img src={img4} alt="" />
-          <img src={img1} alt="" />
+      <div className="page1right-wrap ">
+        <div className="d-flex page1img-wrap  position-relative">
+          <img
+            src={img1}
+            className="position-absolute carousel-img"
+            style={{ left: "0vw", opacity: "0.8" }}
+            alt=""
+          />
+          <img
+            src={img2}
+            className="position-absolute carousel-img"
+            style={{ left: "60vw", opacity: "0.8" }}
+            alt=""
+          />
+          <img
+            src={img3}
+            className="position-absolute carousel-img"
+            style={{ left: "120vw", opacity: "0.8" }}
+            alt=""
+          />
+          <img
+            src={img4}
+            className="position-absolute carousel-img"
+            style={{ left: "180vw", opacity: "0.8" }}
+            alt=""
+          />
         </div>
       </div>
     </>

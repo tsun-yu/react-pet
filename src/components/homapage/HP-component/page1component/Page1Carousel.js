1fc504d6d16f9b5cf0c0d4cf4003ecee996d2162
Wayne <tsun0227@yahoo.com.tw>
2021-04-26 20:22:35 +0800

    carousel optimize

--- src/components/homapage/HP-component/page1component/Page1Carousel.js
+++ src/components/homapage/HP-component/page1component/Page1Carousel.js
@@ -4,29 +4,20 @@ function Page1Carousel(props) {
   const { img1, img2, img3, img4 } = props;
 
   useEffect(() => {
-    const slr = (id) => document.querySelector(id);
-    let newIndex = 1;
-    let slide = -60;
-
     const interval = setInterval(() => {
       const styles = document.querySelectorAll(".carousel-img");
-      // console.log(styles);
-      styles.forEach((v, i) => {
+      styles.forEach((v) => {
         let opacity = +v.style.opacity;
         let left = +v.style.left.split("vw")[0];
-        console.log(opacity);
-        if (opacity != 0) {
-          left === 0
-            ? (v.style.opacity = "0")
-            : (v.style.left = left - 60 + "vw");
-        } else if (left === 0) {
-          v.style.left = "120vw";
+
+        if (left === 0) {
+          opacity === 0 ? (v.style.left = "120vw") : (v.style.opacity = "0");
         } else {
           v.style.left = left - 60 + "vw";
           v.style.opacity = " 0.8";
         }
       });
-    }, 1800);
+    }, 2000);
 
     return () => {
       clearInterval(interval);
@@ -35,33 +26,31 @@ function Page1Carousel(props) {
 
   return (
     <>
-      <div className="page1right-wrap ">
-        <div className="d-flex page1img-wrap  position-relative">
-          <img
-            src={img1}
-            className="position-absolute carousel-img"
-            style={{ left: "0vw", opacity: "0.8" }}
-            alt=""
-          />
-          <img
-            src={img2}
-            className="position-absolute carousel-img"
-            style={{ left: "60vw", opacity: "0.8" }}
-            alt=""
-          />
-          <img
-            src={img3}
-            className="position-absolute carousel-img"
-            style={{ left: "120vw", opacity: "0.8" }}
-            alt=""
-          />
-          <img
-            src={img4}
-            className="position-absolute carousel-img"
-            style={{ left: "180vw", opacity: "0.8" }}
-            alt=""
-          />
-        </div>
+      <div className="d-flex position-relative carousel-area">
+        <img
+          src={img1}
+          className="position-absolute carousel-img"
+          style={{ left: "0vw", opacity: "0.8" }}
+          alt=""
+        />
+        <img
+          src={img2}
+          className="position-absolute carousel-img"
+          style={{ left: "60vw", opacity: "0.8" }}
+          alt=""
+        />
+        <img
+          src={img3}
+          className="position-absolute carousel-img"
+          style={{ left: "120vw", opacity: "0.8" }}
+          alt=""
+        />
+        <img
+          src={img4}
+          className="position-absolute carousel-img"
+          style={{ left: "180vw", opacity: "0.8" }}
+          alt=""
+        />
       </div>
     </>
   );
